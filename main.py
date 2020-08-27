f1b1b199612d5e1c3d2ae3ec47798573e681b4e1
Alvaro Fernandez <fernandez-alvaro@hotmail.com>
2020-08-27 14:06:26 +0200

    initaal commit

---
+++ main.py
@@ -0,0 +1,78 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+productIDs = ['5630967',
+              '5922068',
+              '2482088',
+              '4663716',
+              '7951987',
+              '11921269']
+
+
+class Article:
+    def __init__(self, title, price, sale):
+        self.title = title
+        self.price = price
+        self.sale = sale
+
+
+def getProductDetailFromURL(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    html = list(soup.children)[1]
+    body = list(html.children)[1]
+    productDetail = body.find('div', class_='productDetail')
+
+    return productDetail
+
+
+def getPrice(productDetail):
+    childrenDivs = productDetail.findChildren('div', recursive=False)
+    priceDiv = childrenDivs[0]
+    price = priceDiv.find('strong').getText()
+
+    return price.strip()
+
+
+def checkIfSaleAndGetOldPrice(productDetail):
+    sale = [False, 0]
+    childrenDivs = productDetail.findChildren('div', recursive=False)
+    priceDiv = childrenDivs[0]
+    spans = priceDiv.findChildren('span')
+    if len(spans) > 1:
+        sale[0] = True
+        sale[1] = spans[1].getText()[-7:].strip()
+
+    return sale
+
+
+def getTitle(productDetail):
+    header = productDetail.findChildren('h1', recursive=False)
+    title = header[0].find('strong').getText()+' '+header[0].find('span').getText()
+
+    return title
+
+
+def buildURL(articleID):
+    pre = "https://www.digitec.ch/de/s1/product/"
+
+    return pre+str(articleID)
+
+
+def buildArticle(url):
+    productDetail = getProductDetailFromURL(url)
+    title = getTitle(productDetail)
+    price = getPrice(productDetail)
+    sale = checkIfSaleAndGetOldPrice(productDetail)
+    return Article(title, price, sale)
+
+
+for p in productIDs:
+    art = buildArticle(buildURL(p))
+    print('Article: ')
+    print(art.title)
+    print(art.price)
+    print(art.sale[0])
+    print(art.sale[1])
+    print('####################')
